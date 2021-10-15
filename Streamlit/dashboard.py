ddb3e501e1411e6f9f3521ce23d817219486ff95
Maycon Felipe Mota <mayconfelipemotamw3@gmail.com>
2021-10-14 23:24:41 -0300

    fix: alterações no dashboard

--- Streamlit/dashboard.py
+++ Streamlit/dashboard.py
@@ -7,36 +7,57 @@ from urllib.error import URLError
 
 @st.cache
 def get_data():
-    df = pd.read_csv("https://raw.githubusercontent.com/FelipeGaleao/CGUFuelStats/master/Data/trusted/fuel_consumption.csv")
+    df = pd.read_csv(
+        "https://raw.githubusercontent.com/FelipeGaleao/CGUFuelStats/master/Data/trusted/fuel_consumption.csv")
     return df.set_index("Vehicle")
 
 
 def get_full_data():
-    df = pd.read_csv("https://raw.githubusercontent.com/FelipeGaleao/CGUFuelStats/master/Data/trusted/fuel_consumption.csv")
+    df = pd.read_csv(
+        "https://raw.githubusercontent.com/FelipeGaleao/CGUFuelStats/master/Data/trusted/fuel_consumption.csv")
     return df
 
 
 try:
     df = get_data()
     df_full = get_full_data()
-    
+    df_full['Driver_Name'] = df_full.Driver_Name.str.slice(start=0, stop=5) + " (" + df_full.Driver_Code.str.slice(start= 0, stop=10) + ")"
     vehicles = st.multiselect(
-        "Selecione um veículo", list(df.index)
+        "Selecione um veículo", list(df.index.drop_duplicates())
     )
     if not vehicles:
         st.error("Selecione ao menos um veículo.")
     else:
-        st.write("### Dados brutos", df_full[df_full['Vehicle'].isin(vehicles)])
+        df_filter = df_full[df_full['Vehicle'].isin(vehicles)]
+        col1, col2, col3, col4 = st.columns(4)
+        with col1:
+            st.metric(label="Média (km/L)",
+                      value=f"{round(df_filter['FCM (km/L)'].mean(), 2)} km/L",)
+        with col2:
+            st.metric(label="Distancia (km)",
+                      value=f"{round(df_filter['Distance (KM)'].sum(), 2)} km",)
 
-        data = df_full[df_full['Vehicle'].isin(vehicles)].groupby(by = df_full['Vehicle'])['Distance (KM)'].sum()
+        with col3:
+            st.metric(label="Total de combustível (L)",
+                    value=f"{round(df_filter['Fuel (Litres)'].sum(), 2)} L",)
+            
+        dfvh = df_full[df_full['Vehicle'].isin(vehicles)].reset_index().set_index('Vehicle')
         
-        st.metric(label="Temperature", value="70 °F", delta="1.2 °F")
+        st.write('## Média por veículo selecionado')
+        st.bar_chart(data=dfvh[['FCM (km/L)']])
+        
+        st.write('## Distância e Média por Motorista')
+        st.dataframe(data=dfvh[['Distance (KM)', 'FCM (km/L)', 'Driver_Name']])
+        
+        st.write(' ## Média por Motorista')
+        dfvh = df_full[df_full['Vehicle'].isin(vehicles)].reset_index().set_index('Driver_Name')
+        # dfvh.groupby(by = 'Vehicle').agg({': ['mean', 'min', 'max']})
+        st.bar_chart(data=dfvh[['FCM (km/L)']])
 
+    # st.write("### Gráfico de comparação de médias", df_full[df_full['Vehicle'].isin(vehicles)])
 
-        st.write("### Gráfico de comparação de médias", df_full[df_full['Vehicle'].isin(vehicles)])
-
-        st.bar_chart(data=data)
-        # st.altair_chart(chart, use_container_width=True)
+    # st.bar_chart(data=data)
+    # st.altair_chart(chart, use_container_width=True)
 except URLError as e:
     st.error(
         """
